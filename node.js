a04759efb629ecd6cfc88a8fd6e0d8df1f801c7f
M0H1K4N <nejer911@gmail.com>
2024-01-24 12:23:34 +0400

    codeacademy

--- node.js
+++ node.js
@@ -1,10 +1,33 @@
-const os = require("os");
+// const os = require("os");
 
-// NOW WE know how to use os
-const local = {
-    "home dir": os.homedir(),
-    "Operating System": os.type(),
-    "Last Reboot": os.uptime(),
-}
+// // NOW WE know how to use os
+// const local = {
+//     "home dir": os.homedir(),
+//     "Operating System": os.type(),
+//     "Last Reboot": os.uptime(),
+// }
 
-console.log(local)
+// console.log(local)
+
+
+function getUser (id, callback) {
+    return setTimeout(() => {
+      if (id === 5) {
+        callback(null, { nickname: 'Teddy' })
+      } else {
+        callback(new Error('User not found'))
+      }
+    }, 1000)
+  }
+  
+  function callback (error, user) {
+    if (error) {
+      console.error(error.message)
+      process.exit(1)
+    }
+  
+    console.log(`User found! Their nickname is: ${user.nickname}`)
+  }
+  
+  getUser(1, callback) // -> `User not found`
+  getUser(5, callback) // -> `User found! Their nickname is: Teddy`
