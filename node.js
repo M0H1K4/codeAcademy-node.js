e6b3bd3eab9d80ed3a7e39cc53c6fc45cfda0164
M0H1K4N <nejer911@gmail.com>
2024-01-25 19:03:40 +0400

    Add

--- node.js
+++ node.js
@@ -9,7 +9,6 @@
 
 // console.log(local)
 
-
 // function getUser (id, callback) {
 //     return setTimeout(() => {
 //         if (id === 5) {
@@ -25,13 +24,12 @@
 //         console.error(error.message)
 //         process.exit(1)
 //     }
-    
+
 //     console.log(`User found! Their nickname is: ${user.nickname}`)
 // }
 
 // getUser(1, callback) // -> `User not found`
 // getUser(5, callback) // -> `User found! Their nickname is: Teddy`
-
 
 // const util = require('util');
 // const getUserPromise = util.promisify(getUser);
@@ -47,14 +45,12 @@
 // getUser(1) // -> `User not found`
 // getUser(5) // -> `User found! Their nickname is: Teddy`
 
-
-
 // const getTrailDistancePromise = util.promisify(getTrailDistance);
 
 // getTrailDistancePromise('North Country')
-//   .then((foundTrail) => {      
+//   .then((foundTrail) => {
 //     const nickname = foundTrail.nickname;
-//     const mi = foundTrail.miles; 
+//     const mi = foundTrail.miles;
 //     console.log(`The ${nickname} is ${mi} miles long!`);
 //   })
 //   .catch((error) => {
@@ -62,3 +58,37 @@
 //   })
 
 ///
+
+const events = require("events");
+const fs = require("fs");
+
+// EventEmitter object is used to assign listener functions triggered on specific events
+let myEmitter = new events.EventEmitter();
+
+let newWelcomeMessage = (data) => {
+  console.log(`Welcome to Nodejs, what's your name? ${data}.`);
+};
+
+myEmitter.on("welcome", newWelcomeMessage);
+myEmitter.emit("welcome", "Codey");
+
+// Buffer objects are used to handle binary data
+const buffer = Buffer.from("Hello World");
+console.log(buffer);
+console.log(buffer.toString());
+
+let readDataCallback = (err, data) => {
+  if (err) {
+    console.log(`Something went wrong: ${err}`);
+  } else {
+    console.log(`Provided file contained: ${data}`);
+  }
+};
+
+// file.txt file is loaded using the readFile method of the fs module
+fs.readFile("./file.txt", "utf-8", readDataCallback);
+
+// setImmediate function runs immediately after the current poll phase is completed
+setImmediate(() => {
+  console.log("Welcome to Node.js");
+});
